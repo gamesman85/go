4dd091094d0af9b5c817bdfb39221452a6be1aca
Daniel Andreas Wang <daniel.wang@try.no>
2024-11-23 20:52:05 +0100

    wip

--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	greeting := greet()
+	greeting := greet("en")
 	fmt.Println(greeting)
 }
 
-func greet() string {
-	return "Hello world"
+// language represents the language's code
+type language string
+
+// greet says hello to the world in the specified language
+func greet(l language) string {
+	switch l {
+	case "en":
+		return "Hello world"
+	case "fr":
+		return "Bonjour le monde"
+	default:
+		return ""
+	}
 }
